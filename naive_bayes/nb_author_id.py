32887ac290057f97a6d4e5b16fd1dee23b77eabe
resalisbury <rex.salisbury@gmail.com>
2015-10-18 22:10:57 -0700

    Lesson 1 - naive bayes complete

--- naive_bayes/nb_author_id.py
+++ naive_bayes/nb_author_id.py
@@ -26,7 +26,15 @@ features_train, features_test, labels_train, labels_test = preprocess()
 
 #########################################################
 ### your code goes here ###
+from sklearn.naive_bayes import GaussianNB
+from sklearn.metrics import accuracy_score
+clf = GaussianNB()
+clf.fit(features_train, labels_train)
 
+labels_predict = clf.predict(features_test)
+
+print clf.score(features_test, labels_test)
+print accuracy_score(labels_predict, labels_test)
 
 #########################################################
 
